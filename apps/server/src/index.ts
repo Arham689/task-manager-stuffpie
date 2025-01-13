2de1eb24c69d1fbe68f428a9eb8c040ef727eb48
aashishpanchal <aipanchal51@gmail.com>
2025-01-14 03:55:52 +0530

    fix: error

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,45 +1,42 @@
-import express, { Request, Response ,Router} from 'express';
-import dotenv from 'dotenv';
-import {dbConnect} from "./config/db"
-import authRoutes from './routes/auth.routes'
-import cors from 'cors';
-import cookieParser from 'cookie-parser'; 
-import manageTasks from './routes/manageTasks.route'
-dotenv.config(); 
+import express, { Request, Response, Router } from "express";
+import dotenv from "dotenv";
+import { dbConnect } from "./config/db";
+import authRoutes from "./routes/auth.routes";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import manageTasks from "./routes/manageTasks.route";
+import serveStatic from "./middleware/serve-static";
+dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
 const corsOptions = {
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-    exposedHeaders: ['set-cookie'],
+  origin: "*",
+  credentials: true,
 };
-  
+
+app.use(serveStatic());
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(cookieParser()); 
+app.use(cookieParser());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from Express with TypeScript!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from Express with TypeScript!");
 });
 
-
 if (!process.env.MONGODB_URI) {
-    throw new Error('MONGODB_URI is not defined in environment variables');
+  throw new Error("MONGODB_URI is not defined in environment variables");
 }
 
-dbConnect(process.env.MONGODB_URI)
+dbConnect(process.env.MONGODB_URI);
 
-app.use('/api/v1/auth', authRoutes as Router  );
+app.use("/api/v1/auth", authRoutes as Router);
 
-app.use('/api/v1' , manageTasks as Router )
+app.use("/api/v1", manageTasks as Router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-export default app 
+export default app;
